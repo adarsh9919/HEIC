3fa14a1ee72af04e63b2995df1aaaed997eb83b8
Adarsh Singh <adcric7@gmail.com>
2024-02-22 14:56:24 +0530

    ssnhjhhj
    m

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,6 +17,14 @@ class Footer extends Component {
 
      <div className="row">
         <div className="twelve columns">
+           <ul className="social1-links">
+              {networks}
+           </ul>
+
+           <ul className="copyright">
+              <li>&copy; {copyrights}<a href="https://www.ruralshutterbug.live">{copytag}</a></li>
+              <li>{Design}</li>
+           </ul>
 
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
