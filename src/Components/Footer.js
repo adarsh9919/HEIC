7705a2a17ffa7dc7ecdc5efcf9703c723b3c14a8
Adarsh Singh <adcric7@gmail.com>
2024-02-22 14:36:57 +0530

    ,,,,,,,

---
+++ src/Components/Footer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+
+    if(this.props.data){
+      var copyrights= this.props.data.copyright;
+      var copytag= this.props.data.copyrighttag;
+      var Design= this.props.data.design;
+      var networks= this.props.data.social1.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <footer>
+
+     <div className="row">
+        <div className="twelve columns">
+
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+  </footer>
+    );
+  }
+}
+
+export default Footer;
